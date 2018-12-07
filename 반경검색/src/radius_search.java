243504929c96040e331cd737b7e29c8c643e7f52
나밍 <40882598+snt2525@users.noreply.github.com>
2018-12-07 23:41:19 +0900

    Update radius_search.java

--- 반경검색/src/radius_search.java
+++ 반경검색/src/radius_search.java
@@ -4,17 +4,17 @@ public class radius_search {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();               //전체 POI 개수
-		double[][] poi = new double[n][2];  //POI의 좌표
+		int n = sc.nextInt();                  //전체 POI 개수
+		double[][] poi = new double[n][2];     //POI의 좌표
 		
 		for(int i = 0;i < n; i++) {
 			poi[i][0] = sc.nextDouble();
 			poi[i][1] = sc.nextDouble();
 		}
 		
-		int m = sc.nextInt();               //검색 횟수  
+		int m = sc.nextInt();                   //검색 횟수  
 		for(int i = 0;i < m; i++) {
-			int resultCnt = 0;	   			//일정 반경 안에 포함되는 POI의 총 개수
+			int resultCnt = 0;	   	//일정 반경 안에 포함되는 POI의 총 개수
 			double cX = sc.nextDouble();    //중심 좌표 x
 			double cY = sc.nextDouble();    //중심 좌표 y
 			double cR = sc.nextDouble();    //반경 r
